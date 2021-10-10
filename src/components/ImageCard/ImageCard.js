2a216372ef9affd27b277be164603f70b6ad752b
Ziya Mukhtarov <ziya.mukhtarov@ug.bilkent.edu.tr>
2021-10-10 17:42:59 +0300

    Remove img-card

--- src/components/ImageCard/ImageCard.js
+++ src/components/ImageCard/ImageCard.js
@@ -3,7 +3,7 @@ const ImageCard = (props) => {
   return (
     <div className="col" style={{ maxWidth: "20%" }}>
       <div className="hover-card at-card">
-        <img className="hover-card-img " src={props.src} alt={props.alt} />
+        <img className="hover-card-img" src={props.src} alt={props.alt} />
         <div className="hover-card-title">{props.name}</div>
       </div>
     </div>
--- src/components/PdfViewer/PdfViewer.js
+++ src/components/PdfViewer/PdfViewer.js
@@ -3,7 +3,7 @@ import report from "~/reports/ProjectSpecifications.pdf";
 
 const PdfViewer = () => {
   return (
-    <div className="col-6 img-card">
+    <div className="col-6">
       <div className="at-card report-card">
         <iframe className="iframe-flex" src={report} />
       </div>

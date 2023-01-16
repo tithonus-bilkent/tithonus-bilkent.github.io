f70f5c0a9f60beb6b107a426eb335da9af7e8158
Mokhlaroyim Raupova <48827277+mohlaroyim@users.noreply.github.com>
2023-01-16 10:49:29 +0300

    Hotfix/content (#7)
    * updated for submission
    
    * eslint fix
    
    * package error fix
    
    * updated user manual
    
    * changed video
    
    * user manual fix
    
    * landing and user manual fixes

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -13,24 +13,25 @@ export const Team = () => {
                     <Tooltip title="Ziya Muktharov">
                         <Avatar src="images/ziya.jpg" />
                     </Tooltip>
+                  <Tooltip title="Mannan Abdul">
+                    <Avatar src="images/sunny.jpeg" />
+                  </Tooltip>
                     <Tooltip title="Mokhlaroyim Raupova">
                         <Avatar src="images/mohi.jpg" />
                     </Tooltip>
+                  <Tooltip title="Javid Baghirov">
+                    <Avatar src="images/javid.jpeg" />
+                  </Tooltip>
                     <Tooltip title="Osama Tanveer">
                         <Avatar src="images/osama.jpg" />
                     </Tooltip>
-                    <Tooltip title="Mannan Abdul">
-                        <Avatar src="images/sunny.jpeg" />
-                    </Tooltip>
-                    <Tooltip title="Javid Baghirov">
-                        <Avatar src="images/javid.jpeg" />
+                    <Tooltip title="Haluk Altunel">
+                      <Avatar src="images/haluk.jpeg" />
                     </Tooltip>
                     <Tooltip title="Eray Tüzün">
                         <Avatar src="images/eray.jpeg" />
                     </Tooltip>
-                    <Tooltip title="Haluk Altunel">
-                        <Avatar src="images/haluk.jpeg" />
-                    </Tooltip>
+
                 </AvatarGroup>
             </Box>
             <Box marginTop="10px">
@@ -48,40 +49,40 @@ export const Team = () => {
                         linkedInLink="https://www.linkedin.com/in/ziyaza/"
                         githubLink="https://github.com/ZiyaZa"
                     />
+                  <TeamCard
+                    imgSrc="images/sunny.jpeg"
+                    name="Mannan Abdul"
+                    linkedInLink="https://www.linkedin.com/in/abdul-mannan-0601a2192/"
+                    githubLink="https://github.com/abdulmannan1998"
+                  />
                     <TeamCard
                         imgSrc="images/mohi.jpg"
                         name="Mokhlaroyim Raupova"
                         linkedInLink="https://www.linkedin.com/in/mohlaroyim-raupova-a2a107195/"
                         githubLink="https://github.com/mohlaroyim"
                     />
+                  <TeamCard
+                    imgSrc="images/javid.jpeg"
+                    name="Javid Baghirov"
+                    linkedInLink="https://www.linkedin.com/in/caqqolu/"
+                    githubLink="https://github.com/Jaaw1d"
+                  />
                     <TeamCard
                         imgSrc="images/osama.jpg"
                         name="Osama Tanveer"
                         linkedInLink="https://www.linkedin.com/in/osama-tanveer/"
                         githubLink="https://github.com/osamatanveer"
                     />
-                    <TeamCard
-                        imgSrc="images/sunny.jpeg"
-                        name="Mannan Abdul"
-                        linkedInLink="https://www.linkedin.com/in/abdul-mannan-0601a2192/"
-                        githubLink="https://github.com/abdulmannan1998"
-                    />
-                    <TeamCard
-                        imgSrc="images/javid.jpeg"
-                        name="Javid Baghirov"
-                        linkedInLink="https://www.linkedin.com/in/caqqolu/"
-                        githubLink="https://github.com/Jaaw1d"
-                    />
+                  <TeamCard
+                    name="Haluk Altunel"
+                    imgSrc="images/haluk.jpeg"
+                    linkedInLink="https://www.linkedin.com/in/halukaltunel/"
+                  />
                   <TeamCard
                     name="Eray Tüzün"
                     imgSrc="images/eray.jpeg"
                     linkedInLink="https://www.linkedin.com/in/tuzuneray/"
                     githubLink="https://github.com/eraytuzun"
-                  />
-                  <TeamCard
-                    name="Haluk Altunel"
-                    imgSrc="images/haluk.jpeg"
-                    linkedInLink="https://www.linkedin.com/in/halukaltunel/"
                   />
                 </Box>
             </Box>
